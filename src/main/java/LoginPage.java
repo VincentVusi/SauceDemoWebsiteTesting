7b6ee6132ea9721ff094a1514f2eba34fcf4a47f
VincentVusi <153896296+VincentVusi@users.noreply.github.com>
2023-12-21 08:28:23 +0200

    added java docs to login page

--- src/main/java/LoginPage.java
+++ src/main/java/LoginPage.java
@@ -2,37 +2,73 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+/**
+ * LoginPage class for the website https://www.saucedemo.com/
+ * The LoginPage class represents the page where users can log in.
+ * It contains methods to interact with the login form elements.
+ */
 public class LoginPage {
 
-    WebDriver driver;
+    /**
+     * The WebDriver instance for interacting with the web browser.
+     */
+    private WebDriver driver;
 
+    /**
+     * WebElement representing the username input field.
+     */
     @FindBy(id="user-name")
     private WebElement userName;
 
+    /**
+     * WebElement representing the password input field.
+     */
     @FindBy(id="password")
     private WebElement password;
 
+    /**
+     * WebElement representing the submit button for login.
+     */
     @FindBy(id="login-button")
     private WebElement submit;
 
+    /**
+     * Constructor to initialize the LoginPage with the WebDriver.
+     * @param driver The WebDriver instance to use for interacting with the web browser.
+     */
     public LoginPage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
 
+    /**
+     * Enters the provided username into the username input field.
+     * @param strUserName The username to be entered.
+     */
     public void enterUsername(String strUserName){
         userName.sendKeys(strUserName);
     }
 
+    /**
+     * Enters the provided password into the password input field.
+     * @param strPassword The password to be entered.
+     */
     public void enterPassword(String strPassword){
         password.sendKeys(strPassword);
     }
 
+    /**
+     * Clicks the submit button to log in.
+     */
     public void clickSubmit(){
         submit.click();
     }
 
+    /**
+     * Enters the provided username and password and submits the login form.
+     * @param strUserName The username to be entered.
+     * @param strPassword The password to be entered.
+     */
     public void enterCredentials(String strUserName,String strPassword){
         this.enterUsername(strUserName);
         this.enterPassword(strPassword);
